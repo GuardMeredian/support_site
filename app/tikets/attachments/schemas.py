c8d1751dc4f6278fc12cf236e5b5578c4d4e2bb6
GuardMeredian <sector.zat@gmail.com>
2024-02-20 17:20:53 +0400

    еще один небольшой рефакторинг

--- app/tikets/attachments/schemas.py
+++ app/tikets/attachments/schemas.py
@@ -1,13 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional, List
-from datetime import datetime
+from typing import Optional
 
 class SAttachment(BaseModel):
     id: Optional[int]
     ticket_id: Optional[int]
     filename: Optional[str]
     file_url: str
-    #message_id: Optional[int] = None
+    
 
     class Config:
-        orm_mode = True
+        from_attributes = True
